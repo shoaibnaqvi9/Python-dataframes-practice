0d4cb6a7bc1f6957f704f0e35d1aa9d0d160ab15
Syed Muhammad Shoaib Naqvi <muhshoaib@yahoo.com>
2024-07-08 13:15:46 +0500

    implementing new logics like cumulative statistics, counting, dropping duplicates and counting categorical variables

--- Data manipulation with pandas/Data manipulation with pandas.py
+++ Data manipulation with pandas/Data manipulation with pandas.py
@@ -1,18 +1,32 @@
 import pandas as pd
 
-bio_data={'firstname':['Shoaib','Ali','Ahmed','Fahad','Sohail','Rabia'],
-          'lastname':['Naqvi','Khan','Ali','Mustafa','Khan','Anum'],
-          'gender':['Male','Male','Male','Male','Male','Female'],
-          'Age':[23,21,30,22,25,43],
-          'Height':[6.1,5.7,5.2,5.8,5.7,5.4],
-          'Weight':[70,75,66,80,98,102]
+bio_data={'firstname':['Shoaib','Ali','Ahmed','Fahad','Sohail','Rabia','Fahad','Ali'],
+          'lastname':['Naqvi','Khan','Ali','Mustafa','Khan','Anum','Mustafa','Kamran'],
+          'gender':['Male','Male','Male','Male','Male','Female','Male','Male'],
+          'Age':[21,21,30,21,25,42,30,42],
+          'Height':[6.1,5.7,5.2,5.8,5.7,5.4,6.0,5.6],
+          'Weight':[70,75,66,80,98,102,98,87]
           }
 
 bio_data_frame=pd.DataFrame(bio_data)
-
 print(bio_data_frame)
-print("Mean:\nHeight:",bio_data_frame['Height'].mean(),"Weight:",bio_data_frame['Weight'].mean(),"Age:",bio_data_frame['Age'].mean())
-print("Mode:\nHeight:",bio_data_frame['Height'].mode(),"Weight:",bio_data_frame['Weight'].mode(),"Age:",bio_data_frame['Age'].mode())
-print("Min:\nHeight:",bio_data_frame['Height'].min(),"Weight:",bio_data_frame['Weight'].min(),"Age:",bio_data_frame['Age'].min())
-print("Max:\nHeight:",bio_data_frame['Height'].max(),"Weight:",bio_data_frame['Weight'].max(),"Age:",bio_data_frame['Age'].max())
-print()
+# print(bio_data_frame.head())
+# print(bio_data_frame.info())
+# print("Mean:\nHeight:",bio_data_frame['Height'].mean(),"Weight:",bio_data_frame['Weight'].mean(),"Age:",bio_data_frame['Age'].mean())
+# print("Mode:\nHeight:",bio_data_frame['Height'].mode(),"Weight:",bio_data_frame['Weight'].mode(),"Age:",bio_data_frame['Age'].mode())
+# print("Min:\nHeight:",bio_data_frame['Height'].min(),"Weight:",bio_data_frame['Weight'].min(),"Age:",bio_data_frame['Age'].min())
+# print("Max:\nHeight:",bio_data_frame['Height'].max(),"Weight:",bio_data_frame['Weight'].max(),"Age:",bio_data_frame['Age'].max())
+# print("Sum:\nAge:",bio_data_frame['Age'].sum())
+# print("Quantile:\nAge:",bio_data_frame['Age'].quantile())
+# def pct20(col):
+#     return col.quantile(0.2)
+# print("Aggregate:\nAge:",bio_data_frame['Age'].agg(pct20))
+# print("Cummulative sum:\nAge:",bio_data_frame['Age'].cumsum())
+# print("Cummulative max:\nAge:",bio_data_frame['Age'].cummax())
+# print("Cummulative min:\nAge:",bio_data_frame['Age'].cummin())
+# print("Cummulative prod:\nAge:",bio_data_frame['Age'].cumprod())
+    # unique=bio_data_frame.drop_duplicates(subset=['firstname','lastname'])
+# print(unique)
+# print(bio_data_frame['Age'].value_counts(sort=True))
+print(bio_data_frame['Age'].value_counts(normalize=True))
+
